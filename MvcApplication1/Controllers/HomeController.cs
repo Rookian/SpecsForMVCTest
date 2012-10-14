09cdc353b98d26c45cc3306a8a0d13df909fec67
unknown <Alex@Alex-PC.(none)>
2012-10-14 22:22:51 +0200

    First commit

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using MvcApplication1.Models;
+using NHibernate;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Speichern(Test test)
+        {
+            var isAjaxRequest = HttpContext.Request.IsAjaxRequest();
+            var isValid = ModelState.IsValid;
+            return View("Index");
+        }
+    }
+
+    public class NhibernateController : Controller
+    {
+        public NhibernateController()
+        {
+             ISessionFactory
+        }
+    }
+}
---
+++ MvcApplication1/Global.asax.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using MvcApplication1.Mappings;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace MvcApplication1
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private ISessionFactory _buildSessionFactory;
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            InitializeNHibernate();
+        }
+
+        private void InitializeNHibernate()
+        {
+            var buildConfiguration = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c => c.Database("Testing").Server("localhost").TrustedConnection()))
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TestMap>())
+                .ExposeConfiguration(c => new SchemaExport(c).Execute(true, true, false))
+                .BuildConfiguration();
+
+            _buildSessionFactory = buildConfiguration.BuildSessionFactory();
+        }
+    }
+}
